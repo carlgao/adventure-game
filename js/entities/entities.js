9e8353ba9834a80bab37471f78261bf58bc3d496
Carl Gao <carlgao117@yahoo.com>
2014-12-26 17:48:46 -0800

    character moves and jumps

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -9,13 +9,64 @@ game.PlayerEntity = me.Entity.extend({
     init:function (x, y, settings) {
         // call the constructor
         this._super(me.Entity, 'init', [x, y , settings]);
+		
+		// set the default horizontal and vertical speed (accel vector)
+		this.body.setVelocity(3, 15);
+		
+		// set the display to follow our position on both axes
+		me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
+		
+		// ensure the player is updated even when outside of the viewport
+		this.alwaysUpdate = true;
+		
+		// define a basic walking animation (using all frames)
+		this.renderable.addAnimation("walk", [0, 1, 2, 3, 4, 5, 6, 7]);
+		// define a standing animation (using the first frame)
+		this.renderable.addAnimation("stand", [0]);
+		// set the standing animation as default
+		this.renderable.setCurrentAnimation("stand");
     },
 
     /**
      * update the entity
      */
-    update : function (dt) {
+	update : function (dt) {
+		if (me.input.isKeyPressed('left')) {
+			// flip the sprite on horizontal axis
+			this.renderable.flipX(true);
+			// update the entity velocity
+			this.body.vel.x -= this.body.accel.x * me.timer.tick;
+			// change to the walking animation
+			if (!this.renderable.isCurrentAnimation("walk")) {
+				this.renderable.setCurrentAnimation("walk");
+			}
+		} else if (me.input.isKeyPressed('right')) {
+			// unflip the sprite
+			this.renderable.flipX(false);
+			// update the entity velocity
+			this.body.vel.x += this.body.accel.x * me.timer.tick;
+			// change to the walking animation
+			if (!this.renderable.isCurrentAnimation("walk")) {
+				this.renderable.setCurrentAnimation("walk");
+			}
+		} else {
+			this.body.vel.x = 0;
+			// change to the standing animation
+			this.renderable.setCurrentAnimation("stand");
+		}
 
+		if (me.input.isKeyPressed('jump')) {
+			// make sure we are not already jumping or falling
+			if (!this.body.jumping && !this.body.falling) {
+				// set current vel to the maximum defined value
+				// gravity will then do the rest
+				this.body.vel.y = -this.body.maxVel.y * me.timer.tick;
+				// set the jumping flag
+				this.body.jumping = true;
+			}
+
+		}		
+		
         // apply physics to the body (this moves the entity)
         this.body.update(dt);
 
--- js/resources.js
+++ js/resources.js
@@ -6,6 +6,16 @@ game.resources = [
      */
 	// our level tileset
 	{name: "area01_level_tiles", type: "image", src: "data/img/map/area01_level_tiles.png"},
+	
+	// main player spritesheet
+	{name: "gripe_run_right", type:"image", src: "data/img/sprite/gripe_run_right.png"},
+	
+	// the spinning coin spritesheet
+	{name: "spinning_coin_gold",  type:"image", src: "data/img/sprite/spinning_coin_gold.png"},
+	
+	// our enemty entity
+	{name: "wheelie_right",       type:"image", src: "data/img/sprite/wheelie_right.png"},
+
 
     /* Texture Atlases
      * @example
